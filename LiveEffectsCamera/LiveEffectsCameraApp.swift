9f50a5d9ca6240774122bad5d7f757ef1a9f8036
Vladimir Kostin <diclonius.wolf@gmail.com>
2023-01-10 19:11:50 +0300

    Initial Commit

---
+++ LiveEffectsCamera/LiveEffectsCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveEffectsCameraApp.swift
+//  LiveEffectsCamera
+//
+//  Created by Владимир Костин on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveEffectsCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
